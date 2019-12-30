e5f049381dc9157be56f5f4ac96787ef9beca146
jmvjmv <jmv8928@gmail.com>
2019-12-30 15:22:54 -0500

    Finished 00

--- ActiveRecord_part1/lib/00_attr_accessor_object.rb
+++ ActiveRecord_part1/lib/00_attr_accessor_object.rb
@@ -1,5 +1,17 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+    # This is the getter
+    names.each do |name|
+      define_method(name) do
+        instance_variable_get("@#{name}")
+      end
+
+    #This is the setter 
+      define_method("#{name}=") do |val|
+        instance_variable_set("@#{name}", val)
+      end
+
+    end
   end
 end
