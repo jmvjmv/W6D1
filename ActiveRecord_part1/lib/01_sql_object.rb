3a03bd73b0e3466af3a77c0f5a5bac20766bf7b9
jmvjmv <jmv8928@gmail.com>
2019-12-30 17:30:50 -0500

    Part 1a

--- ActiveRecord_part1/lib/01_sql_object.rb
+++ ActiveRecord_part1/lib/01_sql_object.rb
@@ -12,23 +12,45 @@ class SQLObject
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || self.name.underscore.pluralize
   end
 
+
+  
   def self.all
-    # ...
+    #return an array of all records in the DB
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+    SQL
+
+    parse_all(results)
   end
 
   def self.parse_all(results)
     # ...
+    results.map { |result| self.new(result) }
   end
 
   def self.find(id)
-    # ...
+    # look up a single record by primary key
+    results = DBConnection.execute(<<-SQL, id)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+      WHERE
+        #{table_name}.id = ?
+    SQL
+
+    parse_all(results).first
+
   end
 
   def initialize(params = {})
@@ -44,14 +66,36 @@ class SQLObject
   end
 
   def insert
-    # ...
+    # insert: insert a new row into the table to represent the SQLObject.
+    columns = self.class.columns.drop(1)
+    col_names = columns.map(&:to_s).join(", ")
+    question_marks = (["?"] * columns.count).join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values.drop(1))
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
     # ...
+    set_line = self.class.columns.map { |attr| "#{attr} = ?" }.join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values, id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_line}
+      WHERE
+        #{self.class.table_name}.id = ?
+    SQL
   end
 
   def save
-    # ...
+    id.nil? ? insert : update
   end
 end
