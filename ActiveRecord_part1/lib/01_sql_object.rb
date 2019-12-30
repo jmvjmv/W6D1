dafe9bd250f8e53d435e742667d15a9bc0425fed
jmvjmv <jmv8928@gmail.com>
2019-12-30 17:41:28 -0500

    Finished part 1

--- ActiveRecord_part1/lib/01_sql_object.rb
+++ ActiveRecord_part1/lib/01_sql_object.rb
@@ -6,9 +6,29 @@ require 'active_support/inflector'
 class SQLObject
   def self.columns
     # ...
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+        0
+    SQL
+    cols.map!(&:to_sym)
+    @columns = cols
   end
 
   def self.finalize!
+    self.columns.each do |name|
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
@@ -54,15 +74,23 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      # make sure to convert keys to symbols
+      attr_name = attr_name.to_sym
+      if self.class.columns.include?(attr_name)
+        self.send("#{attr_name}=", value)
+      else
+        raise "unknown attribute '#{attr_name}'"
+      end
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map { |attr| self.send(attr) }
   end
 
   def insert
